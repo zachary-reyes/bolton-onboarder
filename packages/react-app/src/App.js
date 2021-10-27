c930f2eb6f4932b6c69c6a03c78ccc0045bc430a
Dekan <dekanbro@gmail.com>
2021-10-27 08:08:31 -0600

    add address from contract package

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -14,9 +14,19 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { ArrowForwardIcon, CopyIcon, ExternalLinkIcon } from "@chakra-ui/icons";
-import { addresses } from "@project/contracts";
+
 
 import { fetchSafeBalances, fetchSafeIncomingTxs } from "./utils/requests";
+
+const addresses = {
+  molochSummoner: {
+    eth: "0x38064F40B20347d58b326E767791A6f79cdEddCe",
+    xdai: "0x0F50B2F3165db96614fbB6E4262716acc9F9e098",
+    kovan: "0x9c5d087f912e7187D9c75e90999b03FB31Ee17f5",
+    rinkeby: "0xC33a4EfecB11D2cAD8E7d8d2a6b5E7FEacCC521d"
+  },
+  gnosisSafe: "0x3Be7264F5b60635F434559Ab4abc839958768cF2",
+};
 
 function SafeList({ provider }) {
   const [account, setAccount] = useState("");
